99ec9775a7c0d5584762e92908ac2ea595a4ce5e
Jonathan Lee <jonathanlee882@gmail.com>
2023-05-20 13:15:22 +1000

    added map to return all user info

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,39 @@
 // import { useEffect } from "react"
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import axios from "axios"
 
 function Home () {
+    let [users, setUsers] = useState([]);
     async function main() {
-        console.log(await axios.get("https://jsonplaceholder.typicode.com/users"))
+        let { data } = await axios.get("https://jsonplaceholder.typicode.com/users")
+        setUsers(data);
     }
     
     useEffect(() => {
-        main();
+       setTimeout(() => {
+         main();
+       }, 500);
     }, [])
-    return <h1>Home</h1>
+    
+
+
+    if (users.length > 0) {
+        return <div>
+            {users.map((user) => {
+                return (
+                    <div>
+                        <div>{user?.id}</div>
+                        <div>{user?.name}</div>
+                        <div>{user?.email}</div>
+                        <div>{user?.username}</div>
+                    </div>
+                )
+            })}
+        </div>
+        
+    }
+    else return <h1>Loading...</h1>;
 }
+
 
 export default Home
